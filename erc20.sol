cf9af402d3cf817415419f598875e051f288f809
chanderprakash20 <chander132002@gmail.com>
2024-07-14 00:32:53 +0530

    Update Burn Token

--- erc20.sol
+++ erc20.sol
@@ -1,28 +1,50 @@
+
+
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
 
 contract ERC20{
 
-      string constant public name = "dktoken";
-      string constant public symbol = "dkt";
-      uint256 constant public decimals = 18;
-
-      uint256 public totalsupply;
+      string public name;
+      string public symbol;
+      uint256 immutable public decimals;
+     uint256 public totalsupply;
+    address public owner;
 
       event Transfer(address indexed from, address indexed to, uint256 value);
       event Approval(address indexed owner, address indexed spender, uint256 value);
+      event Burn(uint value);
 
       mapping (address => uint256) public balanceof;
 
       mapping (address => mapping(address => uint256)) public allowance;
 
+      constructor(address _owner , string memory _name, string memory _symbol, uint256 _decimals){
+
+        _name = name;
+        _symbol = symbol;
+        _decimals = decimals;
+        _owner = owner;
+
+      }
+
       function transfer(address to, uint256 value, address state) external returns(bool){
            
-         _transfer(msg.sender, to, state, value);
+         _transfer(msg.sender, state, to, value);
 
          return true;
 
+      }
+
+      function _mint(address to, uint256 value) private {
+        balanceof[to] += value;
+        totalsupply += value;
+      }
+
+      function mint(address to, uint256 value) external payable {
+        require(msg.sender == owner);
+        _mint(to, value);
       }
 
 
@@ -39,10 +61,11 @@ contract ERC20{
         function givemetoken() external {
          balanceof[msg.sender] += 1e18;
 
+
         }
 
 
-        function approve(address spender, uint256 value) external returns(bool){
+        function approve(address spender, uint256 value) public returns(bool){
                    
           allowance[msg.sender][spender] += value;
 
@@ -76,5 +99,41 @@ contract ERC20{
 
 
         }
+
+        function deposit(uint value) external payable{
+
+         _mint(msg.sender,value);
+
+         
+          
+       }
+
+
+       function redeem(uint value, address contrct_add) external {
+
+        approve(contrct_add,value);
+                   
+      // require(allowance[msg.sender][contrct_add] >= value, "Allowance exceeded");
+        require(balanceof[msg.sender] >= value, "No money");
+        allowance[msg.sender][contrct_add] -= value;
+
+        _transfer(msg.sender, contrct_add, contrct_add, value);
+
+
+         _burn(value);
+         payable(msg.sender).transfer(value);
+
+                  
+
+       }
+
+       function _burn(uint value) private {
+
+        totalsupply -= value;
+
+        emit Burn(value);
+
+
+       }
 
 }
